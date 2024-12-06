dc36f61e32bce39ee9a81cec1e7d79883e2596d4
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-12-06 15:13:45 +0000

    Update: dated save for csv data

--- helpers/save.py
+++ helpers/save.py
@@ -1,11 +1,12 @@
-import matplotlib.figure
 import os
+
+import matplotlib.figure
+import pandas as pd
 
 from helpers.helpers import (
     get_user_confirm,
     is_png_jpg_jpeg,
     is_valid_file_name,
-    get_hour_str,
     get_date_str,
     path_exists
     )
@@ -20,7 +21,7 @@ def save_dated_figure(fig: object, fig_name: str, file_type: str) -> None:
     base_path = "static"
 
     try:
-        _validate_args(fig, fig_name, file_type)
+        _validate_figure_args(fig, fig_name, file_type)
     except (ValueError, TypeError) as e:
         print_faliure_msg(e)
         return
@@ -43,7 +44,7 @@ def save_dated_figure(fig: object, fig_name: str, file_type: str) -> None:
         return
 
 
-def _validate_args(fig, fig_name, file_type):
+def _validate_figure_args(fig, fig_name, file_type):
     """Raises errors if validation not met"""
     if not isinstance(fig, matplotlib.figure.Figure):
         raise TypeError(
@@ -79,6 +80,7 @@ def _save_figure(figure, file_path):
         if not _overwrite_file_request():
             print("overwrite file aborted.")
             return
+    # figure.savefig(file_path, transparent=True)
     figure.savefig(file_path)
 
 
@@ -100,3 +102,43 @@ def _overwrite_file_request() -> bool:
 def print_faliure_msg(e):
     print(e)
     print("Failed to save file")
+
+
+def save_dated_data_csv(file_data: pd.DataFrame, file_name: str):
+    """try to save data to dated location with data stamp"""
+    base_path = "static"
+
+    try:
+        _validate_dated_data_csv_args(file_data, file_name)
+    except (ValueError, TypeError) as e:
+        print_faliure_msg(e)
+        return
+
+    todays_date = get_date_str()
+    dir_path = _make_path(base_path, todays_date)
+    file_name = file_name + todays_date + ".csv"
+    file_path = _make_path(dir_path, file_name)
+
+    try:
+        _create_todays_dir(dir_path)
+    except OSError as e:
+        print_faliure_msg(e)
+        return
+    
+    try:
+        file_data.to_csv(file_path, index=False)
+    except OSError as e:
+        print_faliure_msg(e)
+        return
+
+def _validate_dated_data_csv_args(file_data: pd.DataFrame, file_name: str) -> None:
+    """raises errors if not of valid args"""
+
+    if not isinstance(file_data, pd.DataFrame):
+        raise TypeError(
+            "data must be pandas.DataFrame "
+            "object not {}".format(type(file_data))
+        )
+    
+    if not is_valid_file_name(file_name):
+        raise ValueError("Invalid file name")
