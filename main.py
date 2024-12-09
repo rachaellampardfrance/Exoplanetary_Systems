7208ed20bdfb03ac7865c9638489aee5bb9d5516
rachaellampardfrance <rachaellampardfrance@gmail.com>
2024-12-09 11:02:52 +0000

    UPDATE: define styles, icons, add template pages...
    Add:
    + favicon and app icons
    + add html templates and call in main.py
    
    Update:
    define basic webpage styles.css

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("home.html")
+
+@app.route("/discoveries")
+def discoveries():
     return render_template("discoveries.html")
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
